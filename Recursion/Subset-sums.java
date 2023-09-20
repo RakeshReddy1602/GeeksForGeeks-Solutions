b29050e8b5350be8079f2987f52fb1fb8b4be26c
RakeshReddy1602 <rakeshb1602@gmail.com>
2023-09-20 21:49:56 +0530

    Pick and non pick method using rescursion solution

--- Recursion/Subset-sums.java
+++ Recursion/Subset-sums.java
@@ -1,1 +1,17 @@
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        ArrayList<Integer> result = new ArrayList<>();
+        getSubsets(0,0,arr,result);
+        result.add(0);
+        return result;
+    }
 
+    void getSubsets(int index,int sum,List<Integer> arr,ArrayList<Integer> result){
+        if(index >= arr.size()){
+            return ;
+        }
+        result.add(sum + arr.get(index));
+        getSubsets(index+1, sum+arr.get(index), arr, result);
+        getSubsets(index+1, sum, arr, result);
+    }
+}
