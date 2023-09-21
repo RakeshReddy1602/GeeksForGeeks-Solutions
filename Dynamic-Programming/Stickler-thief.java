287d5078db1a7c0ea9563e5d5fabeb85dfe76618
RakeshReddy1602 <rakeshb1602@gmail.com>
2023-09-21 17:12:11 +0530

    Memoized solution

--- Dynamic-Programming/Stickler-thief.java
+++ Dynamic-Programming/Stickler-thief.java
@@ -1,1 +1,25 @@
 
+
+class Solution
+{
+   
+    public int FindMaxSum(int arr[], int n)
+    {
+        int[] dp = new int[n];
+        return Math.max(rob(0,arr,dp), rob(1,arr,dp));
+    }
+
+    int rob(int index,int[] arr,int[] dp){
+        
+        if(index >= arr.length){
+            return 0;
+        }
+
+        if(dp[index] != 0){
+            return dp[index];
+        }
+
+        dp[index] = arr[index] + Math.max(rob(index+2, arr, dp), rob(index+3, arr, dp));
+        return dp[index];
+    }
+}
